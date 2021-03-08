fa0b23a2f7bfcc17b67d196ce6c1e1423e754027
Marc Ramis <marc.ramis@enti.cat>
2021-03-08 04:15:11 +0100

    Introduction to Framework

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,11 +1,16 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\glm.hpp>
 
-//Exemple
-extern void Exemple_GUI();
-extern void Exemple_PhysicsInit();
-extern void Exemple_PhysicsUpdate(float dt);
-extern void Exemple_PhysicsCleanup();
+/////////Forward declarations
+namespace LilSpheres {
+	extern const int maxParticles;
+	extern int firstParticleIdx;
+	extern int particleCount;
+	extern void updateParticles(int startIdx, int count, float* array_data);
+}
+
+extern bool renderParticles;
 
 bool show_test_window = false;
 void GUI() {
@@ -14,20 +19,59 @@ void GUI() {
 
 	{	
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
-		Exemple_GUI();
 	}
 	
 	ImGui::End();
 }
 
+class Particle {
+public:
+	glm::vec3 position;
+
+	Particle() : position(glm::vec3(0.f, 0.f, 0.f)) {};
+	Particle(glm::vec3 _position) : position(_position) {};
+};
+
+class ParticleSystem {
+public:
+	ParticleSystem(int numParticles /*, glm::vec3 *position*/) {};
+	
+	void updateParticle(int idx, glm::vec3 newPosition) {
+
+	}
+
+	void updateLilSpheres() 
+	{
+	}
+private:
+};
+
+Particle p1;
+
 void PhysicsInit() {
-	Exemple_PhysicsInit();
+	renderParticles = true;
+	p1 = Particle(glm::vec3(4.f, 3.f, 2.f));
+
+	LilSpheres::firstParticleIdx = 0;
+	LilSpheres::particleCount = 1;
 }
 
 void PhysicsUpdate(float dt) {
-	Exemple_PhysicsUpdate(dt);
+	int start_particle = 0;
+	int number_of_particles = 1;
+	float* particles_position_vector = &(p1.position.x);
+
+	//p1.position = glm::vec3(float(rand() / 5.f), float(rand() / 10.f), float(rand() / 5.f));
+	//p1.position.x = static_cast<float>(rand() / static_cast <float> (5.f));
+	//p1.position.y = static_cast<float>(rand() / static_cast <float> (10.f));
+	//p1.position.z = static_cast<float>(rand() / static_cast < float> (5.f));
+
+	//p1.position.x = -5 + p_pars.min + (float)rand() / (RAND_MAX / (p_pars.max - p_pars.min));
+	//p1.position.x = p_pars.min + (float)rand() / (RAND_MAX / (p_pars.max - p_pars.min));
+	//p1.position.x = -5 + p_pars.min + (float)rand() / (RAND_MAX / (p_pars.max - p_pars.min));
+
+	LilSpheres::updateParticles(start_particle, number_of_particles, particles_position_vector);
 }
 
 void PhysicsCleanup() {
-	Exemple_PhysicsCleanup();
 }
